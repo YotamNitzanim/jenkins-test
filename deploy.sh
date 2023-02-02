e24d47ebf16c8ceeaf02cec3d444bf796fc3318f
Ubuntu <ubuntu@ip-172-31-6-37.ap-northeast-1.compute.internal>
2023-02-02 11:17:56 +0000

    creating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-# wow
+echo "Starting to deploy docker image.."
+DOCKER_IMAGE=yotamnitzanim/nitzanim-ex-01
+docker pull $DOCKER_IMAGE
+docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+docker run -d -p 5000:5000 $DOCKER_IMAGE
